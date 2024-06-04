0f9912a321a27a5a5d0b7a16b9943e3ef5cad2c6
baokhanh1701 <89294586+baokhanh1701@users.noreply.github.com>
2024-06-04 22:18:30 +0700

    fix: api key

--- app/api/send/route.ts
+++ app/api/send/route.ts
@@ -1,7 +1,7 @@
 import { EmailTemplate } from "../../components/EmailTemplate";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API_KEY);
 const fromEmail = process.env.fromEmail;
 
 export async function POST(req: any, res: any) {
